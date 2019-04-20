7ac3ff7138f4d50dceabdd2fdbc2a361429858b5
Joseph Foley <josephfoley@rcn.com>
2019-04-20 18:24:09 -0400

    Update roomgenerator.py

--- roomgenerator.py
+++ roomgenerator.py
@@ -12,12 +12,20 @@ import random;
 
 
 smallmonsters = ["goblin warrior", "kobold miner",
-"fire elemental", "orc grunt", "water elemental"]
+"fire elemental", "orc grunt", "water elemental", "small rat swarm",
+"gargoyle", "goblin archer"]
 
 mediummonsters = ["orc warrior", "kobold slinger", "crimson dragon hatchling"
-,"goblin mob boss", "cat warrior", "cat archer", "steel dragon hatchling"]
+,"goblin mob boss", "cat warrior", "cat archer", "steel dragon hatchling", "large rat swarm",
+"giant rat"]
 
-objects = ["treasure chest", "old map", "trapdoor", "rusty dagger", "spell scroll" ]
+largemonsters = ["orc warlord", "small crimson dragon", "large fire elemental",
+"large water elemental", "demonic guard"]
+
+objects = ["treasure chest", "old map", "trapdoor", "rusty dagger", "spell scroll" ,
+"gold pile", "ball of yarn", "orb of wisdom"]
+
+
 
 def randomchoice(listname):
     gennum = random.randrange(0,(len(listname)-1))
@@ -63,10 +71,25 @@ def generatemediumroom():
             mediumroominfo.append(str(randomchoice(mediummonsters)))
         elif (getnum > 7):
             mediumroominfo.append(str(randomchoice(objects)))
+
     print(mediumroominfo)
-# large room (5-12 entities)
+# large room (7-12 entities)
+
+def generatelargeroom():
+    entitynum = random.randrange(7,12)
+    largeroominfo =[]
+    largeroominfo.append("LARGE ROOM")
+    for i in range(0, entitynum):
+        getnum = random.randrange(0,10)
+        if (getnum ==1):
+            largeroominfo.append(str(randomchoice(smallmonsters)))
+        elif (getnum >1 and getnum <5):
+            largeroominfo.append(str(randomchoice(mediummonsters)))
+        elif (getnum >=5 and getnum < 9):
+            largeroominfo.append(str(randomchoice(largemonsters)))
+        elif (getnum >= 9):
+            largeroominfo.append(str(randomchoice(objects)))
+    print(largeroominfo)
 
 
-generatesmallroom()
-generatesmallroom()
-generatemediumroom()
+
