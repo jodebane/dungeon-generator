3cf733803c8f5a39e29af953f105372d0b5e018b
Joseph Foley <josephfoley@rcn.com>
2019-04-20 10:16:44 -0400

    Update and rename main.py to dungeongenmain.py

--- dungeongenmain.py
+++ dungeongenmain.py
@@ -8,12 +8,25 @@
 # Copyright:   (c) Joseph Foley 2019
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
+import random
 import roomgenerator
 
-roomgenerator.generatemediumroom()
 
+##firststage - genarate series of rooms,
+def stage():
+    smallrooms = random.randrange(2,5)
+    for i in range(0, smallrooms):
+        roomgenerator.generatesmallroom()
 
-##firststage - genarate series of rooms
+    mediumrooms = random.randrange(5,8)
+
+    for i in range(0, mediumrooms):
+        roomgenerator.generatemediumroom()
+
+    largerooms = random.randrange(1,4)
+    for i in range(0, largerooms):
+        roomgenerator.generatelargeroom()
+
 
 ##secondstage - generate boss room or high difficulty encounter
 
@@ -21,3 +34,8 @@ roomgenerator.generatemediumroom()
 
 ##fourth stage - generate boss room or high difficulty encouonter,
 ##with xp and gold rewards
+
+def generatestages():
+    stage()
+##call function
+generatestages()
